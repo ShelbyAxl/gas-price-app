6fe6479d1bf7f2951c821b0607c253bffa09d49e
ShelbyAxl <luisangelin_12@hotmail.com>
2024-01-05 19:33:53 -0700

    commit 8

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -12,7 +12,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="container">
       <div className="flex  bg-zinc-950 rounded-lg overflow-hidden py-8 min-w-[calc(50rem)] h-[calc(100vh-9rem)] max-h-[calc(40rem)]">
-        <section className="flex flex-col w-3/12 gap-4 py-5 min-w-64 border-r-2 border-slate-600">
+        <section className="flex flex-col w-3/12 gap-4 py-5 min-w-64 border-r-2 border-gr border-slate-600 rounded-3xl">
           <h1 className="py-4 px-8 text-4xl text-center"> Dashboard </h1>
           <ul>
             <Link href="/dashboard">
