d6e4325ccc546676d310c9f421349fa1f4a2e914
ShelbyAxl <luisangelin_12@hotmail.com>
2024-01-05 17:38:30 -0700

    listo alv 3

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -12,10 +12,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 
   return (
     <div className="container">
-      <h1 className="font-bold text-4xl my-2">Dashboard</h1>
-      <div className="flex  bg-zinc-950 rounded-lg overflow-hidden py-8 min-w-[calc(50rem)] h-[calc(100vh-13rem)] max-h-[calc(40rem)]">
-        <section className="flex flex-col w-3/12 gap-4 py-5 min-w-64 border-r-2 border-slate-600">.
-          <h1 className="py-4 px-8 text-3xl"> { " " } </h1>
+      <div className="flex  bg-zinc-950 rounded-lg overflow-hidden py-8 min-w-[calc(50rem)] h-[calc(100vh-9rem)] max-h-[calc(40rem)]">
+        <section className="flex flex-col w-3/12 gap-4 py-5 min-w-64 border-r-2 border-slate-600">
+          <h1 className="py-4 px-8 text-4xl text-center"> Dashboard </h1>
           <ul>
             <Link href="/dashboard">
               <li className="py-4 px-4 hover:bg-zinc-900 transition-all">
