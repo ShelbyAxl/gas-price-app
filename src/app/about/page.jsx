757cf60775151c72da9fd0ecae774902e0fe444e
ShelbyAxl <luisangelin_12@hotmail.com>
2024-01-05 22:29:49 -0700

    commit 11

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -6,11 +6,11 @@ function AboutPage() {
   return (
     <div className="h-[calc(100vh-12rem)] flex items-center">
       <div className="px-10 flex flex-col gap-y-10 items-center">
-        <Image src={GasStation} width={200} height={200} alt="" />
+        <Image src={GasStation} width={300} height={300} alt="" />
         <h1 className="text-6xl w-[28rem]">Quienes somos?</h1>
       </div>
       <div>
-        <p className="text-lg px-10 text-center">
+        <p className="text-2xl px-10">
           En GasPrice, nos enorgullece ofrecer a nuestros usuarios una
           herramienta sencilla y eficiente para acceder a información
           actualizada sobre los precios de la gasolina en las estaciones más
@@ -18,10 +18,7 @@ function AboutPage() {
         <br />
         <br />
           Nuestra misión es facilitar a los conductores la toma de decisiones
-          informadas sobre dónde llenar el tanque al mejor precio posible. Nos
-          esforzamos por ofrecer una plataforma fácil de usar que te permita
-          encontrar rápidamente las gasolineras más cercanas y comparar los
-          precios para que puedas ahorrar dinero en cada viaje.
+          informadas sobre dónde llenar el tanque al mejor precio posible.
         </p>
       </div>
     </div>
