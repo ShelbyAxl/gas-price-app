e7f16d531c47af5b57ef90cb3a27b665aa7f329e
ShelbyAxl <luisangelin_12@hotmail.com>
2024-01-05 21:46:58 -0700

    commit 10

--- src/components/StationCard.jsx
+++ src/components/StationCard.jsx
@@ -1,21 +1,21 @@
 import Link from "next/link";
 
-function StationCard({ props }) {
+function StationCard({ station }) {
   return (
     <Link href="/">
       <div
         className="bg-slate-800 rounded-md hover:cursor-pointer hover:bg-slate-700 transition-all text-white py-3 px-4 overflow-hidden"
       >
-        <h3 className="font-bold text-lg"> {props.stationName} </h3>
+        <h3 className="font-bold text-lg"> {station.stationName} </h3>
         <div className="flex gap-10">
           <h1 className=" text-4xl py-3 text-green-600">
-            $ {props.gasPriceGreen}
+            $ {station.gasPriceGreen}
           </h1>
           <h1 className=" text-4xl py-3 text-red-600">
-            $ {props.gasPriceRed}
+            $ {station.gasPriceRed}
           </h1>
         </div>
-        <p> {props.address} </p>
+        <p> {station.address} </p>
       </div>
     </Link>
   );
