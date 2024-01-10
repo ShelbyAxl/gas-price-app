8cc58242fc43af16c7b49a7eac90e3b9969394e3
ShelbyAxl <luisangelin_12hotmail.com>
2024-01-10 13:13:39 -0700

    commit 27

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -4,7 +4,7 @@ import GasStation from "@/images/gas-station.png";
 
 function AboutPage() {
   return (
-    <div className="h-[calc(100vh-12rem)] flex flex-col lg:flex-row items-center">
+    <div className="h-full flex flex-col lg:flex-row items-center">
       <div className="px-10 flex flex-col gap-y-10 items-center mb-10 lg:mb-0 ">
         <Image src={GasStation} width={300} height={300} alt="" />
         <h1 className="text-6xl w-[28rem]">Quienes somos?</h1>
